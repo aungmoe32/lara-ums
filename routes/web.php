708c6aa626f2fbe88ef3144a6908e58372af170a
Aung Moe Myint Thu <aungmoemyintthu@gmail.com>
2025-03-02 10:40:50 +0630

    Add Laravel Breeze for authentication and scaffolding

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-
-
-
-
-
-
-
-Route::resource('roles', App\Http\Controllers\RoleController::class);
-
-Route::resource('permissions', App\Http\Controllers\PermissionController::class);
-
-Route::resource('features', App\Http\Controllers\FeatureController::class);
-
-Route::resource('users', App\Http\Controllers\UserController::class);
-
-
-Route::resource('roles', App\Http\Controllers\RoleController::class);
-
-Route::resource('permissions', App\Http\Controllers\PermissionController::class);
-
-Route::resource('features', App\Http\Controllers\FeatureController::class);
-
-Route::resource('users', App\Http\Controllers\UserController::class);
+require __DIR__.'/auth.php';
