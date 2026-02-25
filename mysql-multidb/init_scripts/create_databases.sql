1168780c565a1bb412a1df53046c620fdab739d4
Aung Moe Myint Thu <aungmoemyintthu@gmail.com>
2026-02-25 14:17:04 +0630

    feat: Add Bugsink service to Docker Compose and create its database initialization script.

---
+++ mysql-multidb/init_scripts/create_databases.sql
@@ -0,0 +1,6 @@
+-- create primary database
+CREATE DATABASE IF NOT EXISTS `bugsink`;
+-- You can also create users and grant permissions here if needed
+-- GRANT ALL PRIVILEGES ON primary_db.* TO 'root'@'%';
+-- GRANT ALL PRIVILEGES ON secondary_db.* TO 'root'@'%';
+
